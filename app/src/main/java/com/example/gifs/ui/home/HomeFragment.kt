07de9a0f67b266ef778a4649c10c8d902a8b43ed
alexa <panina.Alexa5@gmail.com>
2022-06-14 17:46:15 +0300

    ++

--- app/src/main/java/com/example/gifs/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gifs/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.example.gifs.R
 import com.example.gifs.data.network.ClientGiphy
+import com.example.gifs.data.network.GifResponse
 import com.example.gifs.databinding.FragmentHomeBinding
 import com.example.gifs.ui.home.items.GifItem
 import com.xwray.groupie.GroupAdapter
@@ -32,12 +33,35 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? {
         bindingHome = FragmentHomeBinding.inflate(inflater, container, false)
-
         // получить gif
         val groupAdapter = GroupAdapter<GroupieViewHolder>()
+
+        var items = listOf<GifItem>(
+            GifItem(
+                GifResponse(
+                    "1",
+                    "https://media.giphy.com/media/hpAtD9u0KVR9NSiLBt/giphy.gif",
+                    "gif-1"
+                )
+            ),
+            GifItem(
+                GifResponse(
+                    "2",
+                    "https://media.giphy.com/media/QXgrWK8kP866nCxdLe/giphy.gif",
+                    "gif-2"
+                )
+            ),
+            GifItem(
+                GifResponse(
+                    "3",
+                    "https://media.giphy.com/media/UDU4oUJIHDJgQ/giphy.gif",
+                    "gif-3"
+                )
+            )
+        )
+
+        groupAdapter.addAll(items)
         bindingHome.rvTrending.adapter = groupAdapter
-
-
         /*client.api.getTrending()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
